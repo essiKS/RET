05cbe0a6e7518aaf3fd223c7477cacdc91dd25e9
Essi Kujansuu <you@example.com>
2020-08-03 22:42:55 +0300

    rid of mother consumer

--- auctionone/otree_extensions/consumers.py
+++ auctionone/otree_extensions/consumers.py
@@ -1,31 +1,89 @@
 # First changed
-from consumers import _OTreeJsonWebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 from auctionone.models import Player, Group, JobOffer
 from otree.models import Participant
 from otree.models_concrete import ParticipantToPlayerLookup
 import logging
-# ADDED
+from django.conf import settings
 import json
 from asgiref.sync import async_to_sync
-
 logger = logging.getLogger(__name__)
 
 ALWAYS_UNRESTRICTED = 'ALWAYS_UNRESTRICTED'
 UNRESTRICTED_IN_DEMO_MODE = 'UNRESTRICTED_IN_DEMO_MODE'
 
 
-class GeneralTracker(_OTreeJsonWebsocketConsumer):
-    unrestricted_when = 'ALWAYS_UNRESTRICTED'
+class GeneralTracker(JsonWebsocketConsumer):
+    # Imposes a structure
+    logger = logging.getLogger(__name__)
 
-    def group_name(self, group_id, participant_code):
-        return "RETplayer-" + str(participant_code)
+    def clean_kwargs(self, **kwargs):
+        '''
+        subclasses should override if the route receives a comma-separated params arg.
+        otherwise, this just passes the route kwargs as is (usually there is just one).
+        The output of this method is passed to self.group_name(), self.post_connect,
+        and self.pre_disconnect, so within each class, all 3 of those methods must
+        accept the same args (or at least take a **kwargs wildcard, if the args aren't used)
+        '''
+        return kwargs
 
-    def get_player(self):
-        return Player.objects.get(id=self.player_pk)
+    def group_name(self, **kwargs):
+        raise NotImplementedError()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cleaned_kwargs = self.clean_kwargs(**self.scope['url_route']['kwargs'])
+        group_name = self.group_name(**self.cleaned_kwargs)
+        self.groups = [group_name] if group_name else []
+
+    def connection_groups(self, **kwargs):
+        group_name = self.group_name(**self.cleaned_kwargs)
+        return [group_name]
+
+    unrestricted_when = ''
+
+    # there is no login_required for channels
+    # so we need to make our own
+    # https://github.com/django/channels/issues/1241
+    def connect(self):
+        AUTH_LEVEL = settings.AUTH_LEVEL
+
+        auth_required = (
+                (not self.unrestricted_when)
+                and AUTH_LEVEL
+                or self.unrestricted_when == UNRESTRICTED_IN_DEMO_MODE
+                and AUTH_LEVEL == 'STUDY')
+
+        if auth_required and not self.scope['user'].is_staff:
+            msg = 'rejected un-authenticated access to websocket path {}'.format(
+                self.scope['path']
+            )
+            logger.warning(msg)
+            # consider also self.accept() then send error message then self.close(code=1008)
+            # this only affects otree core websockets.
+        else:
+            # need to accept no matter what, so we can at least send
+            # an error message
+            self.accept()
+            self.post_connect(**self.cleaned_kwargs)
+
+    def post_connect(self, **kwargs):
+        pass
+
+    def disconnect(self, message, **kwargs):
+        self.pre_disconnect(**self.cleaned_kwargs)
+
+    def pre_disconnect(self, **kwargs):
+        pass
+
+    def receive_json(self, content, **etc):
+        self.post_receive_json(content, **self.cleaned_kwargs)
+
+    def post_receive_json(self, content, **kwargs):
+        pass
 
 
 class TaskTracker(GeneralTracker):
-
     unrestricted_when = 'ALWAYS_UNRESTRICTED'
     # url_pattern = r'^/auction_one_tasktracker/(?P<participant_code>.+)$'
 
@@ -39,16 +97,13 @@ class TaskTracker(GeneralTracker):
             'participant_code': participant_code,
         }
 
-    # ADDED
     def group_name(self, participant_code):
         return "RETplayer-" + str(participant_code)
 
-    # ADDED
     def RET_message(self, event):
         # Send message to WebSocket
         self.send(text_data=json.dumps(event))
 
-    # ADDED IS this doing anything but harm?
     def post_connect(self, participant_code):
         # add them to the channel_layer
         self.room_group_name = self.group_name(participant_code)
@@ -89,7 +144,6 @@ class TaskTracker(GeneralTracker):
 
 
 class AuctionTracker(GeneralTracker):
-
     unrestricted_when = 'ALWAYS_UNRESTRICTED'
 
     def clean_kwargs(self, params):
@@ -98,10 +152,17 @@ class AuctionTracker(GeneralTracker):
         cur_page_index = participant._index_in_pages
         lookup = ParticipantToPlayerLookup.objects.get(participant=participant, page_index=cur_page_index)
         self.player_pk = lookup.player_pk
+        print("at kwargs group id", group_id, "code", participant_code)
         return {
-            'group_id': int(group_id),
+            'group_id': group_id,
             'participant_code': participant_code,
         }
+
+    def group_name(self, group_id, participant_code):
+        return "RETplayer-" + str(participant_code)
+
+    def get_player(self):
+        return Player.objects.get(id=self.player_pk)
 
     def get_group(self):
         player = self.get_player()
--- minimum/otree_extensions/consumers.py
+++ minimum/otree_extensions/consumers.py
@@ -1,12 +1,7 @@
-# CHANGED: copied the consumers from otree.channels into the parent folder
-from consumers import _OTreeJsonWebsocketConsumer
-# recopied the above line
-
-# we need to import our Player model to get and put some data there
+from channels.generic.websocket import JsonWebsocketConsumer
 from minimum.models import Player
 from asgiref.sync import async_to_sync
-
-# ADDED
+from django.conf import settings
 import json
 import logging
 logger = logging.getLogger(__name__)
@@ -14,8 +9,77 @@ logger = logging.getLogger(__name__)
 ALWAYS_UNRESTRICTED = 'ALWAYS_UNRESTRICTED'
 UNRESTRICTED_IN_DEMO_MODE = 'UNRESTRICTED_IN_DEMO_MODE'
 
+class GeneralTracker(JsonWebsocketConsumer):
+    # Imposes a structure
+    logger = logging.getLogger(__name__)
 
-class Minimum(_OTreeJsonWebsocketConsumer):
+    def clean_kwargs(self, **kwargs):
+        '''
+        subclasses should override if the route receives a comma-separated params arg.
+        otherwise, this just passes the route kwargs as is (usually there is just one).
+        The output of this method is passed to self.group_name(), self.post_connect,
+        and self.pre_disconnect, so within each class, all 3 of those methods must
+        accept the same args (or at least take a **kwargs wildcard, if the args aren't used)
+        '''
+        return kwargs
+
+    def group_name(self, **kwargs):
+        raise NotImplementedError()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cleaned_kwargs = self.clean_kwargs(**self.scope['url_route']['kwargs'])
+        group_name = self.group_name(**self.cleaned_kwargs)
+        self.groups = [group_name] if group_name else []
+
+    def connection_groups(self, **kwargs):
+        group_name = self.group_name(**self.cleaned_kwargs)
+        return [group_name]
+
+    unrestricted_when = ''
+
+    # there is no login_required for channels
+    # so we need to make our own
+    # https://github.com/django/channels/issues/1241
+    def connect(self):
+        AUTH_LEVEL = settings.AUTH_LEVEL
+
+        auth_required = (
+                (not self.unrestricted_when)
+                and AUTH_LEVEL
+                or self.unrestricted_when == UNRESTRICTED_IN_DEMO_MODE
+                and AUTH_LEVEL == 'STUDY')
+
+        if auth_required and not self.scope['user'].is_staff:
+            msg = 'rejected un-authenticated access to websocket path {}'.format(
+                self.scope['path']
+            )
+            logger.warning(msg)
+            # consider also self.accept() then send error message then self.close(code=1008)
+            # this only affects otree core websockets.
+        else:
+            # need to accept no matter what, so we can at least send
+            # an error message
+            self.accept()
+            self.post_connect(**self.cleaned_kwargs)
+
+    def post_connect(self, **kwargs):
+        pass
+
+    def disconnect(self, message, **kwargs):
+        self.pre_disconnect(**self.cleaned_kwargs)
+
+    def pre_disconnect(self, **kwargs):
+        pass
+
+    def receive_json(self, content, **etc):
+        self.post_receive_json(content, **self.cleaned_kwargs)
+
+    def post_receive_json(self, content, **kwargs):
+        pass
+
+
+class Minimum(GeneralTracker):
     unrestricted_when = ALWAYS_UNRESTRICTED
 
     # General variables for example for identification cleaned
